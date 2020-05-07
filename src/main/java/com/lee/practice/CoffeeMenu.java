7299faccf6f6d17df5f5b0c414b81ed3260b16da
LeeJeongSeok <ljs0429777@gmail.com>
2020-05-08 01:50:06 +0900

    메뉴판 객체 생성 및 테스트 코드 작성

---
+++ src/main/java/com/lee/practice/CoffeeMenu.java
@@ -0,0 +1,15 @@
+package com.lee.practice;
+
+public class CoffeeMenu {
+
+    public void showMenu() {
+        System.out.println("===============================================================");
+        System.out.println("===                                                         ===");
+        System.out.println("===                Vending Machine V1 입니다.                ===");
+        System.out.println("===                 원하시는 메뉴를 입력해주세요.                  ===");
+        System.out.println("===                 1. 아메리카노 2. 카페 라떼 3. 카라멜 마끼야또    ===");
+        System.out.println("===                                                         ===");
+        System.out.println("===                                                         ===");
+        System.out.println("===============================================================");
+    }
+}
---
+++ src/test/java/com/lee/practice/MenuTest.java
@@ -0,0 +1,12 @@
+package com.lee.practice;
+
+import org.junit.Test;
+
+public class MenuTest {
+
+    @Test
+    public void showMenu() throws Exception {
+        CoffeeMenu coffeMenu = new CoffeeMenu();
+        coffeMenu.showMenu();
+    }
+}
