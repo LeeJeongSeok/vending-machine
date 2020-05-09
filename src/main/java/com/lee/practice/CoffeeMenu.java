2bffae6026c22a1480d54cab47877e493107b79f
LeeJeongSeok <ljs0429777@gmail.com>
2020-05-09 21:57:21 +0900

    showMenu 출력 내용 수정

--- src/main/java/com/lee/practice/CoffeeMenu.java
+++ src/main/java/com/lee/practice/CoffeeMenu.java
@@ -4,12 +4,9 @@ public class CoffeeMenu {
 
     public void showMenu() {
         System.out.println("===============================================================");
-        System.out.println("===                                                         ===");
-        System.out.println("===                Vending Machine V1 입니다.                ===");
-        System.out.println("===                 원하시는 메뉴를 입력해주세요.                  ===");
-        System.out.println("===                 1. 아메리카노 2. 카페 라떼 3. 카라멜 마끼야또    ===");
-        System.out.println("===                                                         ===");
-        System.out.println("===                                                         ===");
+        System.out.println("Vending Machine v1 입니다.              ");
+        System.out.println("원하시는 메뉴를 선택하여 입력해주세요.          ");
+        System.out.println("1. 아메리카노, 2. 카페 라때, 3. 카라멜 마끼야또");
         System.out.println("===============================================================");
     }
 }
