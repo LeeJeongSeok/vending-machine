12ed747578bf7f626f94e1861cfc85b3b2542d11
LeeJeongSeok <ljs0429777@gmail.com>
2020-05-13 19:07:09 +0900

    VendingMachineTest 테스트 코드 리펙토링

--- src/test/java/com/lee/practice/VendingMachineTest.java
+++ src/test/java/com/lee/practice/VendingMachineTest.java
@@ -8,36 +8,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class VendingMachineTest {
 
+    private VendingMachine machine;
+
+    @Before
+    public void setUp() {
+        machine = new VendingMachine(10000);
+    }
+
     @Test
     public void VendingMachine_생성후_현재_투입된_금액_표시() {
-        VendingMachine machine = new VendingMachine(10000);
         assertThat(10000).isEqualTo(machine.currentMoney());
     }
 
     @Test
     public void VendingMachine에_남아있는_잔돈반환() {
-
-        //given
-        VendingMachine machine = new VendingMachine(5800);
-
-        //when
-        machine.returnChange();
-
-        //that
         assertThat(machine.returnChange()).isEqualTo(machine.currentMoney());
     }
 
     @Test
     public void VendingMachine에서_음료값을_지불() {
 
-        //given
-        VendingMachine machine = new VendingMachine(10000);
-
-        //when
-        int returnChange = machine.getOrderDrink(5000);
-
-        //that
-        assertThat(returnChange).isEqualTo(machine.currentMoney());
+        assertThat(machine.getOrderDrink(4000)).isEqualTo(machine.currentMoney());
     }
 
 
